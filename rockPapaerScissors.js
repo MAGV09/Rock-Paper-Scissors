c89baaaf29753a4ff7bb7936ac95befb3b8b7c27
MAGV09 <mohamedadel2002320@gmail.com>
2025-08-13 12:46:56 +0300

    add initial ui

--- rockPapaerScissors.js
+++ rockPapaerScissors.js
@@ -1,59 +1,83 @@
 'use strict';
 
+const hero = document.querySelector('#hero');
+const container = document.querySelector('#container');
+const display = document.querySelector('#display');
+const roundDetails = document.createElement('div');
+const button = document.createElement('button');
+hero.appendChild(button);
+button.textContent = 'play again';
+button.classList.add('hide');
+hero.insertBefore(roundDetails, container);
+
+hero.insertBefore(roundDetails, container);
+
 const getComuterChoice = function () {
   const randomNum = Math.floor(Math.random() * 3) + 1;
   const computerChoice =
     randomNum === 1 ? 'rock' : randomNum === 2 ? 'paper' : 'scissors';
   return computerChoice;
 };
-getComuterChoice();
-
-const getHumanChoice = function () {
-  let humanChoice = prompt('choose rock,paper or scissors').toLowerCase();
-  if(humanChoice==='rock'||humanChoice==='paper'||humanChoice==='scissors'){
-    return humanChoice
-  } 
-  else{
-    alert(`invalid choice please enter a correct one `);
-   return getHumanChoice()
-  }
-};
-
-let computerSelection = getComuterChoice;
-let humanSelection = getHumanChoice;
 
 function playGame() {
   let computerScore = 0;
   let humanScore = 0;
-  const playRound = function (computerChoice, humanChoice) {
+  let roundCount = 0;
+  container.addEventListener('click', (e) => {
+    switch (e.target.id) {
+      case 'rock':
+        playRound(getComuterChoice(), 'rock');
+        break;
+      case 'paper':
+        playRound(getComuterChoice(), 'paper');
+
+        break;
+      case 'scissors':
+        playRound(getComuterChoice(), 'scissors');
+
+        break;
+    }
+  });
+
+  function playRound(computerChoice, humanChoice) {
+    roundDetails.textContent = '';
+
     if (
       (computerChoice === 'rock' && humanChoice === 'scissors') ||
       (computerChoice === 'paper' && humanChoice === 'rock') ||
       (computerChoice === 'scissors' && humanChoice === 'paper')
     ) {
       computerScore++;
-      console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+      roundDetails.textContent = `You Lost the round! ${computerChoice} beats ${humanChoice}`;
     } else if (computerChoice === humanChoice) {
-      console.log('draw');
+      roundDetails.textContent = `Draw! you both chose ${humanChoice}`;
     } else {
       humanScore++;
-      console.log(`You Won the round!`);
-      console.log(
-        `computer chose ${computerChoice} while you chose ${humanChoice}`
-      );
+
+      roundDetails.textContent = `You Won the round!
+       computer chose ${computerChoice} while you chose ${humanChoice} `;
     }
-  };
-  for (let i = 0; i < 5; i++) {
-    console.log(`Round:${i + 1}`);
-    playRound(getComuterChoice(), getHumanChoice());
-  }
-  if (computerScore > humanScore) {
-    console.log(`you have lost to the cpu (${humanScore}-${computerScore}) `);
-  } else if(computerScore < humanScore){
-    console.log(`You won (${humanScore}-${computerScore})`);
-  }
-  else{
-    console.log(`Draw! (${humanScore}-${computerScore})`);
+    display.textContent = `${humanScore} : ${computerScore}  `;
+    roundCount++;
+    if (roundCount === 5) {
+      container.classList.toggle('hide');
+      button.classList.toggle('hide');
+      button.addEventListener('click', () => {
+        container.classList.toggle('hide');
+        roundDetails.textContent = '';
+        display.textContent = '';
+        button.classList.toggle('hide');
+
+        playGame();
+      });
+      if (computerScore > humanScore) {
+        roundDetails.textContent = `you have lost to the cpu (${humanScore}-${computerScore}) `;
+      } else if (computerScore < humanScore) {
+        roundDetails.textContent = `You won (${humanScore}-${computerScore})`;
+      } else {
+        roundDetails.textContent = `Draw! (${humanScore}-${computerScore})`;
+      }
+    }
   }
 }
 playGame();
